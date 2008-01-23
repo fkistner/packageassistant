7d74cbf36a6316fbee95c2a99790a9edbad9b2ce
Facundo Carreiro <fcarreiro@3b6ecb2f-fb6e-24fa-c9fb-45acc951bd93>
2008-01-23 23:05:00 +0000

    BSD License.

--- PackageAssistant/src/PackageDependency.h
+++ PackageAssistant/src/PackageDependency.h
@@ -1,4 +1,10 @@
-/* PackageDependency */
+/*
+Copyright (c) 2008, PackageAssistant contributors.
+All rights reserved. See CONTRIBUTORS file for the list of authors.
+This piece of software is distributed under the NEW BSD License.
+You should have a LICENSE file along with this distribution
+look at it for more information.
+*/
 
 #import <Cocoa/Cocoa.h>
 
--- PackageAssistant/src/PackageStateTransformer.m
+++ PackageAssistant/src/PackageStateTransformer.m
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2008, PackageAssistant contributors.
+All rights reserved. See CONTRIBUTORS file for the list of authors.
+This piece of software is distributed under the NEW BSD License.
+You should have a LICENSE file along with this distribution
+look at it for more information.
+*/
+
 #import "PackageStateTransformer.h"
 
 @implementation PackageStateTransformer
